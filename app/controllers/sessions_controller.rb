a29ccb9ddd99c1b61cb57ba0e8f2c2253fba9e2a
Rehan Raza <rhn.raza121@gmail.com>
2020-04-23 20:51:58 +0530

    Add login page,create and destroy session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+    def new
+
+    end
+
+    def create
+        user= User.find_by(email: params[:sessions][:email].downcase)
+        if user && user.authenticate(params[:sessions][:password])
+            session[:user_id]=user.id
+            flash[:notice]="Logged in successfully"
+            redirect_to user
+        else
+            flash.now[:alert] = "Invalid username or password"
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id]=nil
+        flash[:notice]="Logged out"
+        redirect_to root_path
+    end
+end
